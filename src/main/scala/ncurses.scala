67e4a018b4381349b8f022579e21d3d47498d72c
Alexey Alekhin <laughedelic@gmail.com>
2015-10-05 17:48:28 +0200

    Upgraded to the latest statika (new-instructions)

--- src/main/scala/ncurses.scala
+++ src/main/scala/ncurses.scala
@@ -2,12 +2,8 @@ package ohnosequencesBundles.statika
 
 import ohnosequences.statika._, bundles._, instructions._
 
-
 case object ncurses extends Bundle() {
 
-  def install: Results = {
-    Seq("yum", "install", "-y", "ncurses*") ->-
-    success(s"${bundleName} is installed")
-  }
+  def instructions: AnyInstructions = cmd("yum")("install", "-y", "ncurses*")
 
 }
